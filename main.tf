d1bb5278403906ed8e67c181d77b409bbc0fbf31
sangwooB <sangwoo.bae@bespinglobal.com>
2022-11-04 18:19:05 +0900

    all

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
